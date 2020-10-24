b58bd3f1b1afc2c7c8d16003d8ab6223af546e06
Ori Markowitz <ori.markowitz@gmail.com>
2020-10-24 09:33:02 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,4 +7,6 @@ def join_nested_strings(src)
     element_index = 0
     while element_index < src[row_index].length do
       if src[row_index][element_index] == typeof string
+        results_array << src[row_index][element_index]
+        element_index += 1
 end
